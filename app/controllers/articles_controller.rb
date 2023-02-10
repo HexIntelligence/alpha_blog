1a2f2a0a79288b21f31ba7bcb098c7ae48d5aa54
Ebenezer Ankrah <ebenankrah@gmail.com>
2023-02-09 20:34:43 -0500

    Add editing & updating article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,37 @@ end
 
 
 def new
+  @article = Article.new
 end 
+
+
+def edit
+ 
+  @article = Article.find(params[:id])
+
+end
 
 def create
 # render plain: params[:article]
 @article = Article.new(params.require(:article).permit(:title, :description))
 # render plain: @article.inspect
-@article.save
+if @article.save
+  flash[:notice] = "Article was created successfully."
+redirect_to @article
+else
+render 'new'
+end
+end
+
+def update
+  # byebug
+  @article = Article.find(params[:id])
+  if @article.update(params.require(:article).permit(:title, :description))
+flash[:notice] = "Article was updated successfully."
+redirect_to @article
+  else
+render 'edit'
+  end
 end
 
 end
