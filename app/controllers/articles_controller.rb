08a3a2cceb89e4db4fc56cdc3d6fb8d94b2c093a
Ebenezer Ankrah <ebenankrah@gmail.com>
2023-03-03 16:44:12 -0500

    Add login page

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,71 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
+def show 
+  # byebug
+end
+
+def index
+  # @articles = Article.all
+  @articles = Article.paginate(page: params[:page], per_page: 5)
+end 
+
+
+def new
+  @article = Article.new
+end 
+
+
+def edit
+ 
+
+end
+
+def create
+# render plain: params[:article]
+@article = Article.new(article_params)
+@article.user = User.first
+# render plain: @article.inspect
+if @article.save
+  flash[:notice] = "Article was created successfully."
+redirect_to @article
+else
+render 'new'
+end
+end
+
+def update
+  # byebug
+  if @article.update(article_params)
+flash[:notice] = "Article was updated successfully."
+redirect_to @article
+  else
+render 'edit'
+  end
+end
+
+def destroy
+  @article.destroy
+  redirect_to articles_path
+#   if @article.update(params.require(:article).permit(:title, :description))
+# flash[:notice] = "Article was updated successfully."
+# redirect_to @article
+#   else
+# render 'edit'
+#   end
+# end
+end 
+
+private   #anything below this is a private method that can only be used in this controller
+
+def set_article
+@article = Article.find(params[:id])
+end
+
+def article_params
+  params.require(:article).permit(:title, :description)
+end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+ resources :articles
+# resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'ebby', to: 'pages#ebby'
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create' --replaced with above since we get all restful apis & route
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+end
