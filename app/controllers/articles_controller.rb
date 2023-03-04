a905fe54609b1d965b24cf366584de840ae43732
Ebenezer Ankrah <ebenankrah@gmail.com>
2023-03-04 16:38:43 -0500

    Restrict actions for articles & users controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,71 +1,78 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
-
-def show 
-  # byebug
-end
-
-def index
-  # @articles = Article.all
-  @articles = Article.paginate(page: params[:page], per_page: 5)
-end 
-
-
-def new
-  @article = Article.new
-end 
-
-
-def edit
- 
-
-end
-
-def create
-# render plain: params[:article]
-@article = Article.new(article_params)
-@article.user = current_user
-# render plain: @article.inspect
-if @article.save
-  flash[:notice] = "Article was created successfully."
-redirect_to @article
-else
-render 'new'
-end
-end
-
-def update
-  # byebug
-  if @article.update(article_params)
-flash[:notice] = "Article was updated successfully."
-redirect_to @article
-  else
-render 'edit'
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  def show 
+    # byebug
   end
-end
 
-def destroy
-  @article.destroy
-  redirect_to articles_path
-#   if @article.update(params.require(:article).permit(:title, :description))
-# flash[:notice] = "Article was updated successfully."
-# redirect_to @article
-#   else
-# render 'edit'
-#   end
-# end
-end 
+  def index
+    # @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end 
 
-private   #anything below this is a private method that can only be used in this controller
 
-def set_article
-@article = Article.find(params[:id])
-end
+  def new
+    @article = Article.new
+  end 
 
-def article_params
-  params.require(:article).permit(:title, :description)
-end
+
+  def edit
+  
+
+  end
+
+  def create
+  # render plain: params[:article]
+  @article = Article.new(article_params)
+  @article.user = current_user
+  # render plain: @article.inspect
+  if @article.save
+    flash[:notice] = "Article was created successfully."
+  redirect_to @article
+  else
+  render 'new'
+  end
+  end
+
+  def update
+    # byebug
+    if @article.update(article_params)
+  flash[:notice] = "Article was updated successfully."
+  redirect_to @article
+    else
+  render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  #   if @article.update(params.require(:article).permit(:title, :description))
+  # flash[:notice] = "Article was updated successfully."
+  # redirect_to @article
+  #   else
+  # render 'edit'
+  #   end
+  # end
+  end 
+
+  private   #anything below this is a private method that can only be used in this controller
+
+  def set_article
+  @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
 
   def show
     # @user = User.find(params[:id]) this is removed because it's set at the top & set_user method at bottom
@@ -50,4 +53,12 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
+  end
+  
 end
