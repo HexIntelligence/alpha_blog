3b06053df7ccf0a5fd73bf014de9693092166131
Ebenezer Ankrah <ebenankrah@gmail.com>
2023-03-04 23:20:36 -0500

    Add admin user access

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_27_190829) do
+ActiveRecord::Schema.define(version: 2023_03_05_030701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2023_02_27_190829) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "pasword_digest"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
